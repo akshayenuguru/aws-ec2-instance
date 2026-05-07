f4d57c92de4881b8880c786a28e98177b2f10c89
akshayenuguru <akshay@rafay.co>
2026-05-07 12:43:59 +0530

    Define AWS variables for infrastructure provisioning
    Added multiple variables for AWS configuration including region, AMI ID, instance type, and names for tagging.

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-west-2"
+}
+
+variable "aws_region_az" {
+  description = "AWS region AZ"
+  default = "us-west-2b"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-0854e54abaeae283b"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t3.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "madhu1-infra-provisioner"
+}
+variable "name1" {
+  description = "name to pass to Name1 tag"
+  default = "madhu2-infra-provisioner"
+}
+variable "email" {
+  description = "email"
+  default = "automation@rafay.co"
+}
