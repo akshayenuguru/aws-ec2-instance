43f4ad5a79e7c2b6be8b2b15d541bbe4f6653e69
akshayenuguru <akshay@rafay.co>
2026-05-07 12:42:56 +0530

    Add Terraform configuration for AWS instances

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      env = "qa"
+      email       = "madhu@rafay.co"
+    }
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name = var.name
+    env = "qa"
+    email       = var.email
+  }
+  volume_tags = {
+    env = "qa"
+    email       = "madhu@rafay.co"
+  }
+}
+
+resource "aws_instance" "ubuntu-1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name = var.name1
+    env = "qa"
+    email       = var.email
+  }
+  volume_tags = {
+    env = "qa"
+    email       = "madhu@rafay.co"
+  }
+  depends_on = [aws_instance.ubuntu]
+}
